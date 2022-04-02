03ed81e725cccfad9b90db46c169efef218d0b1a
matsui-kento <puma0813abc@gmail.com>
2022-04-02 18:27:51 +0900

    Problem1の解答
    FizzBuzzを拡張する

--- Problem1/node/index.js
+++ Problem1/node/index.js
@@ -1,19 +1,44 @@
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
 
-const server = http.createServer((req, res)=>{
-   const queryObject = url.parse(req.url, true).query;
-   console.log(queryObject.obj);
+const server = http.createServer((req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Request-Method", "*");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.writeHead(200, { "Content-Type": "text/html" });
 
-   // ここに処理を記述してください。
-   ans = "answer";
+  let answer = [];
+  let data = "";
+  let obj = {};
 
-   res.setHeader('Access-Control-Allow-Origin', '*');
-   res.setHeader('Access-Control-Request-Method', '*');
-   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
-   res.setHeader('Access-Control-Allow-Headers', '*');
-   res.writeHead(200, {'Content-Type': 'text/html'});
-   res.write(ans);
-   res.end();
+  req.on("data", (chunk) => {
+    data += chunk;
+  });
+
+  req.on("end", () => {
+    if (data) {
+      obj = JSON.parse(data).obj;
+      for (let number = 1; number < 31; number++) {
+        let matchText = [];
+        for (const pattern of obj) {
+          if (number % pattern.num == 0) {
+            matchText.push(pattern.text);
+          }
+        }
+
+        if (matchText.length) {
+          answer.push(matchText.join(""));
+        } else {
+          answer.push(number.toString());
+        }
+      }
+
+      res.write(answer.join(", "));
+      res.end();
+    } else {
+      res.end();
+    }
+  });
 });
-server.listen(8080); 
+server.listen(8080);
